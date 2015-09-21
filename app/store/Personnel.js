1e4456a735794b01a4f39460bed58df964fc3661
Vera Barstad <verab12@student.uia.no>
2015-09-21 13:52:17 +0200

    Change

--- app/store/Personnel.js
+++ app/store/Personnel.js
@@ -8,14 +8,7 @@ Ext.define('TutorialApp.store.Personnel', {
         'name', 'email', 'phone'
     ],
      autoLoad: true,
-/*
-    data: { items: [
-        { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
-        { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
-        { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
-        { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
-    ]},
-*/
+
     proxy: {
         type: 'ajax',
 
@@ -24,12 +17,6 @@ Ext.define('TutorialApp.store.Personnel', {
             type: 'json',
             rootProperty: 'items'
         }
-        //type: 'memory',
 
-        //reader: {
-            //type: 'json',
-            //rootProperty: 'items'
-
-        //}
     }
 });
